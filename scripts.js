2f487aba0f0e85509b68b6eae3484946f7aa960d
madKULOLO <41076023+madKULOLO@users.noreply.github.com>
2025-01-10 00:27:02 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ function updateBackButton(event) {
 }
 
 async function checkStreamStatus() {
-    const channelName = "deflixnps";
+    const channelName = "madkulolo";
     const clientId = "gp762nuuoqcoxypju8c569th9wz7q5"; 
     const accessToken = "nicnpw2xfm36f0fewnz1dtzww9i3hj"; 
 
